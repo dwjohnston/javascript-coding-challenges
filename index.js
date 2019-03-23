9c838232ab5e5f6c8bffd349639418da6bf9e9ce
David Johnston <david@blacksheepcode.com>
2019-03-23 12:24:17 +1100

    Add two new examples

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ function adder(a, b) {
 /**
  * Write a program that returns an array numbers from 1 to 20. But for multiples of three return "Fizz" instead of the number and for the multiples of five return "Buzz". For numbers which are multiples of both three and five return "FizzBuzz".
  */
-function fizzBuzz(n) {
+function fizzBuzz() {
+
+}
+
+/*
+    eg. isSquare(15) === false
+    isSquare(25) === true
+*/
+function isSquare(n) {
+
+}
+
+/**
+ * eg findLargest([1, 99, 4]) === 99
+ */
+function findLargest(array) {
 
 }
 
