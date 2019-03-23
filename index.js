eb1f41bf419bc022fc5876d9d11a096961d8d84d
Ryota H <grateful2all@gmail.com>
2019-03-23 13:54:47 +1100

    Update

--- index.js
+++ index.js
@@ -33,17 +33,76 @@ function fizzBuzz() {
 /*
     eg. isSquare(15) === false
     isSquare(25) === true
-    isSquare(-1) === false (negative numbers always return false)
 */
 function isSquare(n) {
+    switch (n % 10) {
+        case 2:
+        case 3:
+        case 7:
+        case 8:
+        return false;
+    }
 
+
+    // switch (n % 10){
+    //     case 0:
+    //     if(n === 0 || n >= 100 && n % 10 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 1:
+    //     if(n % 1 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 2:
+    //     if(n % 4 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 3:
+    //     if(n % 9 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 4:
+    //     if(n % 6 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 5:
+    //     if(n >= 25 && n % 5 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 6:
+    //     if(n >= 36 && n % 6 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 7:
+    //     if(n >= 49 && n % 9 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 8:
+    //     if(n >= 64 && n % 4 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     case 9:
+    //     if(n >= 81 && n % 1 === 0){
+    //         return true;
+    //     } else {return false;}
+    //     default:
+    //     return false;
+
+    // }
+    
 }
 
 /**
  * eg findLargest([1, 99, 4]) === 99
  */
 function findLargest(array) {
-
+    var largest = array[0];
+    for (var i = 0; i < array.length; i++){
+        if(largest < array[i]) {
+            largest = array[i];
+        }
+    }
+    return largest;
 
 }
 
@@ -52,6 +111,6 @@ module.exports = {
     sanityTestFunction,
     adder,
     fizzBuzz,
-    isSquare,
     findLargest,
+    isSquare,
 }
