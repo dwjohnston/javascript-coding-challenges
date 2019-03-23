068cac68af17c485f5b037193766153bb3671f4b
Ryota H <grateful2all@gmail.com>
2019-03-23 12:25:34 +1100

    Solved Fizz Buzz

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ function adder(a, b) {
  * Write a program that returns an array numbers from 1 to 20. But for multiples of three return "Fizz" instead of the number and for the multiples of five return "Buzz". For numbers which are multiples of both three and five return "FizzBuzz".
  */
 function fizzBuzz() {
-
+    var result = [];
+    for (var i = 1; i <= 20; i++){
+        if(i % 15 === 0){
+            result.push("fizzbuzz");
+        } else if (i % 3 === 0) {
+            result.push("fizz");
+        } else if (i % 5 === 0) {
+            result.push("buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
 }
 
 /*
@@ -31,6 +43,7 @@ function isSquare(n) {
  * eg findLargest([1, 99, 4]) === 99
  */
 function findLargest(array) {
+
 
 }
 
